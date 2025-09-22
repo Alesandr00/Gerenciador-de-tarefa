4d4992113be25d5c4fd36fd98da8d2a74c4c2ea6
Alesandro <alesandro.msi.informatica@gmail.com>
2025-09-22 15:11:56 -0300

    Update tarefas.py

--- tarefas.py
+++ tarefas.py
@@ -1,4 +1,5 @@
 from tkinter import messagebox
+import ttkbootstrap as tb
 
 class TarefaApp:
     def __init__(self, root):
@@ -7,10 +8,9 @@ class TarefaApp:
         self.root.title("Gerenciador de Tarefas - Alesandro")
         self.root.state('zoomed')
 
-.
-.
-.
-.
-.
-.
-.    
+    # ... (outros métodos e funcionalidades aqui)
+
+if __name__ == "__main__":
+    root = tb.Window()
+    app = TarefaApp(root)
+    root.mainloop()
