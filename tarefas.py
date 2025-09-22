b62ebb5aeff47a1af4e7935ec224239a77c5c68b
Alesandro <alesandro.msi.informatica@gmail.com>
2025-09-22 15:05:03 -0300

    Create tarefas.py

---
+++ tarefas.py
@@ -0,0 +1,16 @@
+from tkinter import messagebox
+
+class TarefaApp:
+    def __init__(self, root):
+        self.style = tb.Style(theme='flatly')
+        self.root = root
+        self.root.title("Gerenciador de Tarefas - Alesandro")
+        self.root.state('zoomed')
+
+.
+.
+.
+.
+.
+.
+.    
